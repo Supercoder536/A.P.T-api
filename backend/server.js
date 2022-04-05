819dec41548fa2c30dc3d27db1516f1eda7e8ae4
Supercoder536 <61321645+Supercoder536@users.noreply.github.com>
2022-04-05 23:16:01 +0530

    Error Handler Added

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv").config();
 const mainRouter = require("./routes/mainRouter.js");
 const userRouter = require("./routes/userRouter.js");
 const port = process.env.PORT || 5000;
+const errorHandler = require("./middleware/errorMiddleware.js");
 const connectToDB = require("./config/db.js");
 
 const app = express();
@@ -10,6 +11,8 @@ connectToDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(errorHandler);
 
 app.use("/api/areas", mainRouter);
 app.use("/api/users", userRouter);
